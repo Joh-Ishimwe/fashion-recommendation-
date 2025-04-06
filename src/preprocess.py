2c60864ec630d53d1a24b371146d8a9db0563554
Joh-Ishimwe <j.ishimwe3@alustudent.com>
2025-04-06 23:06:46 +0200

    updating

--- src/preprocess.py
+++ src/preprocess.py
@@ -6,7 +6,7 @@ import logging
 import os
 
 try:
-    from imblearn.over_sampling import SMOTE # type: ignore
+    from imblearn.over_sampling import SMOTE 
     SMOTE_AVAILABLE = True
 except ImportError:
     SMOTE_AVAILABLE = False
